84c9d98b6bd53c6f26f0d6ed7fd61b9a939a10b9
Ankit Kumar <trainee02@omniesolutions.com>
2024-01-30 14:04:55 +0530

    dynamicform

--- src/sleepquestion/schema/sleepquestion.schema.ts
+++ src/sleepquestion/schema/sleepquestion.schema.ts
@@ -1,16 +1,17 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import mongoose from "mongoose";
-import {
-  Gender,
-  Numbers,
-  OftenSnore,
-  Options,
-  Patient,
-  Snore,
-  Snoring,
-  StopBreathing,
-} from "../dto/createform.dto";
 
+@Schema()
+class LabelValues {
+  @Prop()
+  id: mongoose.Types.ObjectId;
+
+  @Prop()
+  label: string;
+
+  @Prop()
+  values: string[];
+}
 
 @Schema({ timestamps: true })
 export class SleepQuestion {
@@ -18,172 +19,13 @@ export class SleepQuestion {
   id: mongoose.Types.ObjectId;
 
   @Prop()
-  patientinformation: [
-    {
-      patientfirstname: string;
-      patientmiddlename: string;
-      patientlastname: string;
-    }
-  ];
+  label: string;
 
   @Prop()
-  SleepApeana: [
+  formData: [
     {
-      snoring: [
-        {
-          doyousnoring: boolean;
-          canyoursnoring: boolean;
-        }
-      ];
-
-      stopbreathing: [
-        {
-          stopbreathing: StopBreathing;
-        }
-      ];
-      collarsize: [
-        {
-          gender: Gender;
-        }
-      ];
-
-      bloodpressure: [
-        {
-          treatedforbp: boolean;
-        }
-      ];
-
-      daytimesleepnes: [
-        {
-          ocassionallydoze: string;
-          busyoractive: boolean;
-          drivingorstopped: boolean;
-          totalpoints: number;
-        }
-      ];
-    }
-  ];
-
-  @Prop()
-  Questionnaire: [
-    {
-      awareofsnoring: string;
-      stopbreathewhileasleep: boolean;
-      wakeuptime: string;
-      difficultyinasleep: boolean;
-      hoursofsleep: string;
-      feelrefreshed: boolean;
-      headache: boolean;
-      seenotherdoctors: [
-        {
-          seenotherdoctor: boolean;
-          who: string;
-          when: string;
-        }
-      ];
-      sleeplab: [
-        {
-          study: boolean;
-          when: string;
-        }
-      ];
-      difficultyinnose: boolean;
-      heartirregularities: boolean;
-      highbp: boolean;
-      lossofmemory: boolean;
-      depressed: boolean;
-      normalbedtime: string;
-      normalwakeuptime: string;
-      schedulechange: boolean;
-      paininterfere: boolean;
-      fallenasleep: [
-        {
-          fallen: boolean;
-          times: string;
-        }
-      ];
-    }
-  ];
-
-  @Prop()
-  AdultChildQuestionnaire: [
-    {
-      patient: Patient;
-    }
-  ];
-
-  @Prop()
-  AdultSleepQuestionnaire: [
-    {
-      dob: Date;
-      age: number;
-      gender: Gender;
-      weight: number;
-      height: number;
-      bmi: string;
-      sleeptest: {
-        sleeptest: boolean;
-        when: string;
-      };
-      diagnosed: boolean;
-      usecpap: boolean;
-      happywithcpap: boolean;
-      happywithsleepappliance: {
-        happy: boolean;
-        notwhy: string;
-      };
-      getoutbednight: string;
-      feeltired: boolean;
-      habituallysnore: boolean;
-      highbp: boolean;
-      wakingheadache: boolean;
-      fatigue: boolean;
-      nasalpassage: boolean;
-      stopbreathingnight: boolean;
-      choking: boolean;
-      grindteeth: boolean;
-      neckcircumference: boolean;
-      bmimorethan35: boolean;
-    }
-  ];
-
-  @Prop()
-  EpworthScale: [
-    {
-      sittingandreading: Numbers;
-      watchingtv: Numbers;
-      sittinginpublic: Numbers;
-      withoutbreak: Numbers;
-      lyingdown: Numbers;
-      sittingandtalking: Numbers;
-      sittingquietly: Numbers;
-      intraffic: Numbers;
-      totalscore: Number;
-    }
-  ];
-
-  @Prop()
-  BerlinQuentionnaire: [
-    {
-      category1: {
-        snore: Snore;
-        snoring: Snoring;
-        oftensnore: OftenSnore;
-        botheredpeople: Snore;
-        anyonenoticedstopbreathing: OftenSnore;
-      };
-
-      category2: {
-        tired: OftenSnore;
-        wakingtimetired: OftenSnore;
-        noddedoff: Options;
-        oftenoccur: OftenSnore;
-      };
-
-      category3: {
-        highbp: Snore;
-        date: Date;
-      };
+      label: string;
+      subQuestion: LabelValues[];
     }
   ];
 }
